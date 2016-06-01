fbb04544b003097fb5b083a8323ca095ea438494
Tanbui <hoangtan223@gmail.com>
2016-06-01 20:12:39 +0700

    Generate Migration for adding column "view_count" to Articles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160531160215) do
+ActiveRecord::Schema.define(version: 20160601131055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -21,6 +21,7 @@ ActiveRecord::Schema.define(version: 20160531160215) do
     t.text     "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "view_count"
   end
 
 end
