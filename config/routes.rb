bb5ec9bc832ead680205f62f3368d78c520a6e5c
Tanbui <hoangtan223@gmail.com>
2016-06-26 16:45:33 +0700

    Add rspec-rails gem

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-    post 'comments' => 'comments#create'
+  post 'comments' => 'comments#create'
+
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
